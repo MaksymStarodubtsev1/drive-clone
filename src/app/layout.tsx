12a4a0b49a5b5f8e5a5195c0e567a53275361769
Max <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2025-05-13 15:55:41 +0300

    update: project description update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import { Inter } from "next/font/google"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Google Drive Clone",
-  description: "A Google Drive UI clone built with Next.js",
+  title: "Folder storage",
+  description: "A folder storage built with Next.js",
 }
 
 export default function RootLayout({
