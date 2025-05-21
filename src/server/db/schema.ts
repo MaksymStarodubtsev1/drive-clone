cc526549bb392aec90fb312b0703bde553109fc0
Max <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2025-05-21 22:49:39 +0300

    fix: correct type for table keys

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,21 +1,21 @@
-import {int, text, singlestoreTableCreator, index} from "drizzle-orm/singlestore-core";
+import {int, text, singlestoreTableCreator, index, bigint} from "drizzle-orm/singlestore-core";
 
 const createTable = singlestoreTableCreator((tableName) => `drive-store_${tableName}`)
 
 export const files = createTable("files_table", {
-    id: int("id").primaryKey().autoincrement(),
+    id: bigint("id", {mode: "bigint", unsigned: true}).primaryKey().autoincrement(),
     name: text("name").notNull(),
-    parentId: int("parent_id").notNull(),
     url: text("url").notNull(),
     size: int("size").notNull(),
+    parentId: bigint("parent_id", {mode: "number", unsigned: true}).notNull(),
 }, (table) => {
     return [index("parent_index").on(table.parentId)]
 })
 
 export const folders = createTable("folders_table", {
-    id: int("id").primaryKey().autoincrement(),
+    id: bigint("id", {mode: "bigint", unsigned: true}).primaryKey().autoincrement(),
     name: text("name").notNull(),
-    parentId: int("parent_id"),
+    parentId: bigint("parent_id", {mode: "bigint", unsigned: true}).notNull(),
 }, (table) => {
     return [index("parent_index").on(table.parentId)]
 })
